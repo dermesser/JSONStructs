f573f707a052fe8c9d4d47fcd0dac8225dbf5d77
Lewin Bormann <lewin@lewin-bormann.info>
2023-03-26 10:47:58 +0200

    Add tests for optional fields

--- test/runtests.jl
+++ test/runtests.jl
@@ -23,11 +23,39 @@ end
 function test_parse_2()
     json = "{\"a\": \"Outer Struct\", \"b\": {\"a\": 33, \"b\": 55.55, \"c\": [\"xyz\", \"abc\"]}}"
     have = parse_struct(OuterStruct, json)
-    want = OuterStruct("Outer Struct", TestStruct1(33, 55.55, ["xyz", "abd"]))
-    @show have, want
+    want = OuterStruct("Outer Struct", TestStruct1(33, 55.55, ["xyz", "abc"]))
+    @assert string(have) == string(want) "$have == $want"
+end
+
+# Optional fields
+@json_parseable struct WithOptional
+    a::Optional{Int}
+    b::Optional{TestStruct1}
+end
+
+function test_parse_3()
+    json = "{\"a\": 1234, \"b\": {\"a\": 33, \"b\": 55.55, \"c\": [\"xyz\", \"abc\"]}}"
+    have = parse_struct(WithOptional, json)
+    want = WithOptional(1234, TestStruct1(33, 55.55, ["xyz", "abc"]))
+    @assert string(have) == string(want) "$have == $want"
+end
+
+function test_parse_4()
+    json = "{\"a\": 1234}"
+    have = parse_struct(WithOptional, json)
+    want = WithOptional(1234, nothing)
+    @assert string(have) == string(want) "$have == $want"
+end
+function test_parse_5()
+    json = "{}"
+    have = parse_struct(WithOptional, json)
+    want = WithOptional(nothing, nothing)
     @assert string(have) == string(want) "$have == $want"
 end
 
 println("Starting JSONStructs test")
 test_parse_1()
 test_parse_2()
+test_parse_3()
+test_parse_4()
+test_parse_5()
